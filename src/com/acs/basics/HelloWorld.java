f97357eb61a8974811d0b7b765b9443e33fb77b2
Ashok <ashokkumarm0007@gmail.com>
2020-12-27 00:14:15 -0800

    My First Commit

---
+++ src/com/acs/basics/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.acs.basics;
+
+/**
+ * @author user
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
